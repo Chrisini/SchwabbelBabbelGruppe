8684ab4d9f0284b5738133222a04696c064f873a
fhtw <fhtw@localhost.localdomain>
2017-05-26 16:17:23 +0200

    bout dialog is working with warnings, but still you can open it :b

--- GameJam_Mai/c_client/callback.c
+++ GameJam_Mai/c_client/callback.c
@@ -18,6 +18,7 @@ void callback_highscore(GSimpleAction *action, GVariant *parameter, gpointer dat
 
 void callback_exit(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
+	g_application_quit(data);
 	/*GtkWidget *dialog;
 	GtkWidget *grid;
 	GtkWidget *label;
@@ -46,17 +47,31 @@ void callback_music(GSimpleAction *action, GVariant *parameter, gpointer data)
 	g_print("Music");
 }
 
+void close_dialog(GtkDialog *dialog, gint response_id, gpointer data)
+{
+	gtk_widget_destroy(GTK_WIDGET(dialog));
+
+}
+
+
 void callback_about(GSimpleAction *action, GVariant *parameter, gpointer data)
 {
-	/*GtkWidget *about_window;
-	about_window = gtk_window_new(GTK_WINDOW_POPUP);
-	GtkWidget *about;
-	about = gtk_about_dialog_new ();
-	gtk_about_dialog_set_program_name(GTK_ABOUT_DIALOG(about), "About Hidden Demons");
-	gtk_about_dialog_set_copyright(GTK_ABOUT_DIALOG(about), "Chrisy, Kathi, Sammy, Beety, John");
-	gtk_show_about_dialog(about_window, "program-name", "About Hidden Demons");
-	*/
-	g_print("Music");
+	//GdkPixbuf *pixbuf;
+	GtkWidget *about_dialog;
+	about_dialog = gtk_about_dialog_new();
+	//pixbuf = gtk_pixbuf_new_from_file("images/logo.png", NULL)	;
+	gtk_show_about_dialog(GTK_WINDOW (data),
+			      "program-name", "About Hidden Demons",
+			      "version", "1.0",
+			      "copyright", "Copyright \xc2\xa9 elevariety",
+			      "comments", "A game by some beginner-developers",
+			      //"authors", "chrisy",
+			      //"logo", pixbuf,
+			      "title", "About: Hidden Demons",
+			      NULL);
+
+	g_signal_connect(GTK_DIALOG(about_dialog), "response", G_CALLBACK(close_dialog), NULL);
+	//g_object_unref (pixbuf);
 }
 
 void callback_help(GSimpleAction *action, GVariant *parameter, gpointer data)
