9c054b3482a263242194bbf6b8abea0840a4ce49
fhtw <fhtw@localhost.localdomain>
2017-06-13 21:52:06 +0200

    forgot to add

---
+++ GameJam_Mai/client/move_complete.c
@@ -0,0 +1,13 @@
+#include "header/game_main.h"
+
+
+/*
+ * This function reads all the data, which is send to the server
+ * after clicking the "Move complete Button" (Sidebar)
+ */
+void move_complete(GtkWidget *wid, gpointer data){
+	widgets *a = (widgets *) data;
+
+
+
+}
